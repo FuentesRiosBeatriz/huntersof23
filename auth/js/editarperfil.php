71999ae92bd601c7039fb0f2906ed2cdc677a69b
FuentesRiosBeatriz <201902491@est.umss.edu>
2023-11-13 15:23:43 -0400

    edi

---
+++ auth/js/editarperfil.php
@@ -0,0 +1,26 @@
+<!-- ./auth/editarperfil.php -->
+
+<?php
+include('./auth.php');  // Incluye el archivo de autenticación
+
+if (!$authenticated) {  // Si no está autenticado, redirige a la página de inicio de sesión
+    header("Location: ./login.php");
+}
+
+?>
+<!-- ./auth/editarperfil.php -->
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Asegúrate de incluir las etiquetas meta y enlaces a CSS necesarios -->
+    <title>Editar Perfil - SpottPlay</title>
+    <form action="./auth/handle-edit-profile.php" method="post">
+    <label for="newName">Nuevo Nombre:</label>
+    <input type="text" id="newName" name="newName" required>
+
+    
+
+    <input type="submit" value="Guardar Cambios">
+</form>
+</head>
